3147ca5042aadda0f9b53ec6e5997a50c16bb2e0
Aman <akshaytripathi8493@gmail.com>
2021-07-01 20:52:25 +0530

    Update procfile and server

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
   console.log("Server has been started");
 });
